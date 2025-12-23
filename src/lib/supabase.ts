a53a76a101511bd4f1ec60194c67192bfeabe751
rickbroken <daironquebrada@gmail.com>
2025-12-23 16:22:10 -0500

    feature / se agregan acmbios en variables de entornos para importarlas correctamente

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } = import.meta.env;
+const { VITE_SUPABASE_PROYECT_ID, VITE_SUPABASE_ANON_KEY } = import.meta.env;
 
-const supabaseUrl = `https://${VITE_SUPABASE_URL}.supabase.co`;
+const supabaseUrl = `https://${VITE_SUPABASE_PROYECT_ID}.supabase.co`;
 const supabaseAnonKey = VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
